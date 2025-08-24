c6a197a0ade3542a780fe4ba424d75a3e3ee21d0
Priyanshu11singh <149782715+Priyanshu11singh@users.noreply.github.com>
2025-08-24 18:49:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Home from "./components/Home/Home.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Fruits from "./components/Fruits/Fruits.jsx";
+import Dairy from "./components/Dairy/Dairy.jsx";
+import SeaFood from "./components/SeaFood/SeaFood.jsx";
+import AllProducts from "./components/AllProducts/AllProducts.jsx";
+import Layout from "./components/Layout/Layout.jsx";
+
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "fruits",
+          element: <Fruits />,
+        },
+        {
+          path: "/dairy",
+          element: <Dairy />,
+        },
+        {
+          path: "/seafood",
+          element: <SeaFood />,
+        },
+        {
+          path: "/allproducts",
+          element: <AllProducts />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
+
+export default App;
